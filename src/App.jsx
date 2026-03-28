b5e435346f2790b0679e766bad57dfcb848f4fa7
valera <valera130102@gmail.com>
2026-03-28 14:05:08 +0300

    changeCounter

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([]); 
+   const [todos, setTodos] = useState([]);
 
   const [count, setCount] = useState(0);
   
-  const addTodo = () => { 
-    const text = prompt("Введите задачу"); 
+  const addTodo = () => {
+    const text = prompt("Введите задачу");
     
-    if (!text) return; 
-      
-    const newTodo = { 
-      id: count, 
-      text: text 
+    if (!text) return;
+    
+    const newTodo = {
+      id: count,
+      text: text
     };
     setCount(count => count + 1)
-    setTodos([...todos, newTodo]); 
-  }; 
+    setTodos([...todos, newTodo]);
+
+
+  };
   
-  return ( 
-    <div style={{ padding: "40px" }}> 
-    <h1>Todo List</h1> 
-    <button onClick={addTodo}> Добавить задачу </button> 
-    <ul> {todos.map(todo => ( 
-      <li key={todo.id}> {todo.text} </li> 
+  return (
+    <div style={{ padding: "40px" }}>
+    <h1>Todo List</h1>
+    <button onClick={addTodo}> Добавить задачу </button>
+    <ul> {todos.map(todo => (
+      <li key={todo.id}> {todo.text} </li>
     ))} 
-    </ul> 
-    </div> 
-    ); 
+    </ul>
+    </div>
+  );
 }
 
 export default App
