ee2f3a5336771fe8c74e994f2eb3622354a4c5a9
valera <valera130102@gmail.com>
2026-06-28 16:54:47 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,39 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const array = [1, 2, 3];
 
   const person = {name: "Иван", age: 20 };
 
-  const data = [
+  const [data, setData] = useState([
     "Привет",
     100,
     true,
     person,
     [1, 2, 3],
     null
-];
+  ]);
+
+    /*const handlers = {
+    text: (value) => value.toUpperCase(),
+
+    number: (value) => value + 10,
+
+    user: (value) => ({
+      ...value,
+      age: value.age + 1
+    })
+  };
+
+  function handleUpdate() {
+    setData((prevData) =>
+      prevData.map((item) => ({
+        ...item,
+        value: handlers[item.type](item.value)
+      }))
+    );
+  } */
 
   const listMultyplyFive = () => {
-    /*const five = 5;
-
-    return array.map(item => {
-        return(
-          <div>
-            {item * five}
-            <button>Удалить</button>
-          </div>
-        );
-    });
-    */
-
       return data.map(
           data => {
             if (typeof data === "object" && data !== null && !Array.isArray(data))
@@ -36,6 +43,15 @@ function App() {
                   {data.name}
                 </div>
               )
+            
+              if (typeof data === "string") {
+                return (
+                  <div>
+                    {data}
+                  </div>
+                )
+              }
+
           }
       )
   }
